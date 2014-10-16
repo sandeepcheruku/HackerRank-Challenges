6743d19d8bd1c9d9a4535face3f6f6934432e053
Anish Acharya <anisha@uci.edu>
2014-10-16 04:37:23 -0700

    Create UtopianTree.cpp
    The Utopian tree goes through 2 cycles of growth every year. The first growth cycle occurs during the monsoon, when it doubles in height. The second growth cycle occurs during the summer, when its height increases by 1 meter.
    Now, a new Utopian tree sapling is planted at the onset of the monsoon. Its height is 1 meter. Can you find the height of the tree after N growth cycles?
    
    Input Format
    The first line contains an integer, T, the number of test cases.
    T lines follow. Each line contains an integer, N, that denotes the number of cycles for that test case.
    
    
    Output Format
    For each test case, print the height of the Utopian tree after N cycles.

---
+++ UtopianTree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int height(int n) {
+    return 0;
+}
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << height(n) << endl;
+    }
+}
