7ef72702c820c363cdfa53894a04d1643b8bdcd0
Anish Acharya <anisha@uci.edu>
2015-07-06 04:54:24 -0500

    update

---
+++ Algorithms/UtopianTree.cpp
@@ -0,0 +1,22 @@
+/*
+The Utopian tree goes through 2 cycles of growth every year. 
+The first growth cycle occurs during the spring, when it doubles in height.
+The second growth cycle occurs during the summer, when its height increases by 1 meter.
+Now, a new Utopian tree sapling is planted at the onset of the spring.
+Its height is 1 meter. Can you find the height of the tree after N growth cycles?
+*/
+#include <iostream>
+using namespace std;
+
+int height(int n) {
+    return 0;
+}
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << height(n) << endl;
+    }
+}
